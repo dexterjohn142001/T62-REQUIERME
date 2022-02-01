e721ed27705fdf3508f671c40417746f9e394e5b
Dexter John <76021304+dexterjohn142001@users.noreply.github.com>
2022-02-01 10:24:15 +0800

    Add files via upload

---
+++ Mi2Act1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+  int main() {
+        int num1 = 3000, answer;
+
+        answer = num1 / 60;
+
+        cout << "The Rotation of " << num1 << " per second is " << answer << endl;
+
+		system("PAUSE");
+
+        return 0;
+    }
